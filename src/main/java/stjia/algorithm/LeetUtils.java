7e336fabe0f9c3bb6a142057ee3b8454653d22f8
stjia <shtjia@163.com>
2018-06-22 00:04:37 +0800

    两数相加 链表

--- src/main/java/stjia/algorithm/LeetUtils.java
+++ src/main/java/stjia/algorithm/LeetUtils.java
@@ -1,5 +1,7 @@
 package stjia.algorithm;
 
+import java.util.List;
+
 public class LeetUtils {
 
     public static boolean canJump(int[] nums) {
@@ -22,5 +24,54 @@ public class LeetUtils {
             isCanJumps[i] = isCanjump;
         }
         return isCanJumps[0];
+    }
+
+    /**
+     *两数相加 --  给定两个非空链表来表示两个非负整数。位数按照逆序方式存储，它们的每个节点只存储单个数字。将两数相加返回一个新的链表。
+     * @param l1 第一个链表
+     * @param l2 第二个链表
+     * @return 结果
+     */
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        //链头
+        ListNode prev = new ListNode(0);
+        ListNode head = prev;
+        // 进位
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+
+            ListNode cur = new ListNode(0);
+            int sum = ((l2 == null) ? 0 : l2.val) + ((l1 == null) ? 0 : l1.val) + carry;
+            cur.val = sum % 10;
+            //处理进位
+            carry = sum / 10;
+            prev.next = cur;
+            prev = cur;
+
+            l1 = (l1 == null) ? l1 : l1.next;
+            l2 = (l2 == null) ? l2 : l2.next;
+        }
+        return head.next;
+    }
+
+    //所需数据结构
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        @SuppressWarnings("unused")
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            this.val = x;
+        }
     }
 }
--- src/main/java/stjia/algorithm/MyMain.java
+++ src/main/java/stjia/algorithm/MyMain.java
@@ -1,7 +1,35 @@
 package stjia.algorithm;
 
+import stjia.algorithm.LeetUtils.ListNode;
+
 public class MyMain {
     public static void main(String[] args) {
         System.out.println("hello world");
+        ListNode l1=new ListNode(2);
+        ListNode l2=new ListNode(4);
+        ListNode l3=new ListNode(3);
+        l1.next=l2;
+        l2.next=l3;
+
+        ListNode l4=new ListNode(5);
+        ListNode l5=new ListNode(6);
+        ListNode l6=new ListNode(4);
+        l4.next=l5;
+        l5.next=l6;
+
+        ListNode result = LeetUtils.addTwoNumbers(l1, l4);
+        printListNode(result);
+    }
+
+    private static void printListNode(ListNode node){
+        System.out.print("[");
+        if (node != null) System.out.print(node.val);
+        while (node.next != null) {
+            System.out.print(",");
+            System.out.print(node.val);
+        }
+        System.out.print("]");
     }
 }
+
+
